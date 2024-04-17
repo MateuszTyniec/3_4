294160d15662fa347352224b96154cb9a6ef9d02
MateuszTyniec <161605903+MateuszTyniec@users.noreply.github.com>
2024-04-17 12:58:19 +0000

    3_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,30 @@
-public class Main {
-  public static void main(String[] args) {
-    int rows = 5; 
-    for (int i = 1; i <= rows; i++) {
+public class Main 
+{
+    public static void main(String[] args) 
+  {
+        int wielkosc = 5; 
+        for (int i = 0; i < wielkosc; i++)
+          {
+            System.out.print("*");
+        }
+        System.out.println();
 
-      for (int k = 1; k <= rows - i; k++) {
-        System.out.print(" ");
-      }
+        
+        for (int i = 0; i < wielkosc - 2; i++)
+          {
+            System.out.print("*");
+            for (int j = 0; j < wielkosc - 2; j++) 
+            {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
 
-
-      for (int j = 1; j <= i; j++) {
-        System.out.print("*");
-      }
-      System.out.println();
+        
+        for (int i = 0; i < wielkosc; i++) 
+        {
+            System.out.print("*");
+        }
+        System.out.println();
     }
-  }
 }
